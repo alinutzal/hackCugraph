ec175df28767312483d9b726bff1eb69ae29b32f
xju <xju@cern.ch>
2021-07-21 17:45:02 -0700

    compiles

--- src/misc_utilities.cpp
+++ src/misc_utilities.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <utilities/test_utilities.hpp>
+#include <test_utilities.hpp>
 
 namespace cugraph {
 namespace test {
